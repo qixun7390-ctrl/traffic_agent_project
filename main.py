758a7882c6ce0790e0f83a713e1726e6f1f11934
qixun7390-ctrl <qixun7390@gmail.com>
2026-07-09 00:02:42 +0800

    首次提交

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.api.v1.api import api_router
+
+app = FastAPI(title="Traffic Agent Project Backend")
+
+app.include_router(api_router,prefix="/api/v1")
+
+@app.get("/")
+def root():
+    return {"message":"Traffic Agent backend is running"}
